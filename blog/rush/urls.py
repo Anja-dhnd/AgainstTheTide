1049aaf46d1fc5ea313f8b5cda7400f77c77890b
Anaïs Dhnd <anais.dehand@tuta.io>
2022-04-26 19:55:25 +0200

    deleted add_post stuff

--- blog/rush/urls.py
+++ blog/rush/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
     #    path('', views.home, name='home'),
     path('', HomeView.as_view(), name='home'),
-    path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article_details'),
+
 ]
